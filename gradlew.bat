a813eadec44bbb211ddafce1681781e0c95f5e57
pdeep31 <40391370+pdeep31@users.noreply.github.com>
2022-09-15 16:08:17 +0530

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -32,7 +32,8 @@ echo location of your Java installation.
 goto fail
 
 :findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
+/* set JAVA_HOME=%JAVA_HOME:"=% */
+set JAVA_HOME=/var/jenkins_home/tools/hudson.model.JDK/java11
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto init
